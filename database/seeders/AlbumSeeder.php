f4f9711d0946e9d893ffe286864edc02c5c31c95
Riccardo Basso <riccardo.basso2001@gmail.com>
2025-03-23 19:58:42 +0100

    fix seeder

--- database/seeders/AlbumSeeder.php
+++ database/seeders/AlbumSeeder.php
@@ -13,28 +13,28 @@ class AlbumSeeder extends Seeder
     public function run()
     {
         Album::insert([
-            ['title' => 'Nà Fotografia', 'artist_id' => 1, 'cover_image' => 'path/to/cover1.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Beata Gioventù', 'artist_id' => 1, 'cover_image' => 'path/to/cover1.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Rapp Underground vol.1', 'artist_id' => 1, 'cover_image' => 'path/to/cover1.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Eccomi', 'artist_id' => 1, 'cover_image' => 'path/to/cover1.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Nà Fotografia', 'artist_id' => 1, 'cover_image' => config('app.app_url') . 'assets/images/covers/spagna2024.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Beata Gioventù', 'artist_id' => 1, 'cover_image' => config('app.app_url') . 'assets/images/covers/beata-gioventu.png', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Rapp Underground vol.1', 'artist_id' => 1, 'cover_image' => config('app.app_url') . 'assets/images/covers/rap.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Eccomi', 'artist_id' => 1, 'cover_image' => config('app.app_url') . 'assets/images/covers/default.png', 'created_at' => now(), 'updated_at' => now()],
 
-            ['title' => 'A Nico', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Donne', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Freestyle', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'No go Schei', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Freestyle 2', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'China', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Piedini', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Sborro Controvoglia', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Goodnight', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Fat Boy', 'artist_id' => 2, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'A Nico', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/battis.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Donne', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/donne.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Freestyle', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/freestyle.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'No go Schei', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/no-go-schei.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Freestyle 2', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/freestyle-2.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'China', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/china.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Piedini', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/piedini.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Sborro Controvoglia', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/sborro-controvoglia.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Goodnight', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/goodnight-sara.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Fat Boy', 'artist_id' => 2, 'cover_image' => config('app.app_url') . 'assets/images/covers/fat-boy.jpg', 'created_at' => now(), 'updated_at' => now()],
 
-            ['title' => 'Ma quale idea', 'artist_id' => 3, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Il laboratorio di wolf', 'artist_id' => 3, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Charlie', 'artist_id' => 3, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Dux', 'artist_id' => 3, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Skit', 'artist_id' => 3, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['title' => 'Yume', 'artist_id' => 3, 'cover_image' => 'path/to/cover2.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Ma quale idea', 'artist_id' => 3, 'cover_image' => config('app.app_url') . 'assets/images/covers/che-idea.png', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Il laboratorio di wolf', 'artist_id' => 3, 'cover_image' => config('app.app_url') . 'assets/images/covers/laboratorio.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Charlie', 'artist_id' => 3, 'cover_image' => config('app.app_url') . 'assets/images/covers/charlie.png', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Dux', 'artist_id' => 3, 'cover_image' => config('app.app_url') . 'assets/images/covers/dux.png', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Skit', 'artist_id' => 3, 'cover_image' => config('app.app_url') . 'assets/images/covers/skit.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['title' => 'Yume', 'artist_id' => 3, 'cover_image' => config('app.app_url') . 'assets/images/covers/yume.png', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -13,9 +13,9 @@ class ArtistSeeder extends Seeder
     public function run()
     {
         Artist::insert([
-            ['name' => 'Basso', 'profile_image' => 'path/to/image1.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Braida', 'profile_image' => 'path/to/image2.jpg', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Jack-In', 'profile_image' => 'path/to/image2.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Basso', 'profile_image' => config('app.app_url') . 'assets/images/artists/basso.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Braida', 'profile_image' => config('app.app_url') . 'assets/images/artists/braida.jpg', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Jack-In', 'profile_image' => config('app.app_url') . 'assets/images/artists/techno-bunker.jpg', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
